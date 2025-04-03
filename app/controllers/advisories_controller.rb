7ec2d1e8bb2d7a107afba1afd9df085e5db2be49
Andrew Nesbitt <andrewnez@gmail.com>
2025-04-03 14:10:41 +0100

    Remove unused views

--- app/controllers/advisories_controller.rb
+++
@@ -1,6 +0,0 @@
-class AdvisoriesController < ApplicationController
-  def index
-    @advisories = Advisory.order('published_at DESC').includes(:project)
-    @pagy, @advisories = pagy(@advisories)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       resources :jobs
-      resources :collectives, only: [:index, :show] 
       resources :projects, constraints: { id: /.*/ }, only: [:index, :show] do
         collection do
           get :lookup
@@ -28,8 +27,6 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  resources :advisories, only: [:index]
 
   resources :projects, constraints: { id: /.*/ } do
     collection do
@@ -43,48 +40,17 @@ Rails.application.routes.draw do
       get :advisories
     end
   end
-
-  resources :funders, only: [:index, :show]
-
-  resources :collectives do
-    member do
-      get :funders
-      get :projects
-      get :packages
-      get :issues
-      get :releases
-      get :commits
-      get :advisories
-      get :transactions
-    end
-    collection do
-      get :batch
-    end
-  end
   
   get 'charts/transactions', to: 'charts#transactions', as: :transactions_chart
   get 'charts/issues', to: 'charts#issues', as: :issues_chart
   get 'charts/commits', to: 'charts#commits', as: :commits_chart
   get 'charts/tags', to: 'charts#tags', as: :tags_chart
 
-  resources :sboms
-
   resources :exports, only: [:index], path: 'open-data'
-
-  get '/charts', to: 'collectives#charts'
-
-  get '/audit/user_owners', to: 'audit#user_owners'
-  get '/audit/no_projects', to: 'audit#no_projects'
-  get '/audit/no_license', to: 'audit#no_license'
-  get '/audit/archived', to: 'audit#archived'
-  get '/audit/inactive', to: 'audit#inactive'
-  get '/audit/no_funding', to: 'audit#no_funding'
-  get '/audit/duplicates', to: 'audit#duplicates'
-  get '/audit', to: 'audit#index'
 
   get '/404', to: 'errors#not_found'
   get '/422', to: 'errors#unprocessable'
   get '/500', to: 'errors#internal'
 
-  root "collectives#index"
+  root "projects#index"
 end
