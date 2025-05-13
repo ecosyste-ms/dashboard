4659e77667d48e811b65b39882af30517cfa5010
Martin Wright <martinjwright@gmail.com>
2025-05-13 15:06:12 +0100

    Variabilise some more color variables to access from chart.js
    We can now theoretically change the colour of the charts to match the good/bad/neutral number styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,14 +5,20 @@
 //= require jquery
 
 const rootStyles = getComputedStyle(document.documentElement);
-const esBlack       = rootStyles.getPropertyValue('--es-black').trim();
-const esGreenLight  = rootStyles.getPropertyValue('--es-green-light').trim();
-const esGreenDark   = rootStyles.getPropertyValue('--es-green-dark').trim();
-const esOrangeLight = rootStyles.getPropertyValue('--es-orange-light').trim();
-const esOrangeDark  = rootStyles.getPropertyValue('--es-orange-dark').trim();
-const esPurple      = rootStyles.getPropertyValue('--es-purple').trim();
-const esPurpleLight = rootStyles.getPropertyValue('--es-purple-light').trim();
-const esPurpleDark  = rootStyles.getPropertyValue('--es-purple-dark').trim();
+
+const esPurple              = rootStyles.getPropertyValue('--es-purple').trim();
+const esPurpleLight         = rootStyles.getPropertyValue('--es-purple-light').trim();
+const esPurpleDark          = rootStyles.getPropertyValue('--es-purple-dark').trim();
+const esPurpleTransparent   = rootStyles.getPropertyValue('--es-purple-transparent').trim();
+const esGreen               = rootStyles.getPropertyValue('--es-green').trim();
+const esGreenLight          = rootStyles.getPropertyValue('--es-green-light').trim();
+const esGreenDark           = rootStyles.getPropertyValue('--es-green-dark').trim();
+const esGreenTransparent    = rootStyles.getPropertyValue('--es-green-transparent').trim();
+const esOrange              = rootStyles.getPropertyValue('--es-orange').trim();
+const esOrangeLight         = rootStyles.getPropertyValue('--es-orange-light').trim();
+const esOrangeDark          = rootStyles.getPropertyValue('--es-orange-dark').trim();
+const esOrangeTransparent   = rootStyles.getPropertyValue('--es-orange-transparent').trim();
+const esBlack               = rootStyles.getPropertyValue('--es-black').trim();
 
 // Global style defaults
 Chart.defaults.font.family = "'Inter', sans-serif";
