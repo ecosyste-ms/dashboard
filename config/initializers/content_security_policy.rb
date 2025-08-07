7bd5d7900cf88ab52a9119b60e7d64c53bea9a25
Andrew Nesbitt <andrewnez@gmail.com>
2025-08-07 11:36:34 +0100

    Update CSP to allow https images and *.ecosyste.ms connections

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,6 +5,6 @@ SecureHeaders::Configuration.default do |config|
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
-    connect_src: %w('self')
+    connect_src: %w('self' *.ecosyste.ms)
   }
 end
