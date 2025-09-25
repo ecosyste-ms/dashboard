9627171c15f72978642abf726ce5a2fde0b35fec
Andrew Nesbitt <andrewnez@gmail.com>
2025-09-25 14:03:39 +0100

    Add SSL redirect options for canonical host in production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,9 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = {
+    redirect: { host: ENV.fetch('CANONICAL_HOST', 'dashboards.ecosyste.ms') }
+  }
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
